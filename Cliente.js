41d7023c0b6bf79eae7932683cbf2f2d77cf87c1
sid3027 <136455228+sid3027@users.noreply.github.com>
2025-11-15 09:49:53 -0300

    novo

---
+++ Cliente.js
@@ -0,0 +1,56 @@
+import { Pessoa } from "./Pessoa.js";
+
+export class Cliente extends Pessoa {
+    #pontucaoFidelidade;
+    #limiteCredito;
+    #categoria; //COMUM, VIP, PCD
+
+    constructor(nome, cpf, dtNasc, pontuacaoFidelidade = 0, limiteCredito = 100.0, categoria = "COMUM") {
+        super(nome, cpf, dtNasc);
+        this.pontucaoFidelidade = pontuacaoFidelidade;
+        this.limiteCredito = limiteCredito;
+        this.categoria = categoria.toUpperCase();
+    }
+
+    get pontucaoFidelidade() {
+        return this.#pontucaoFidelidade;
+    }
+    set pontucaoFidelidade(novaPontuacao) {
+        if (novaPontuacao >= 0) {
+            this.#pontucaoFidelidade = novaPontuacao;
+        } else if (this.#pontucaoFidelidade == undefined) {
+            this.#pontucaoFidelidade = 0;
+        }
+    }
+
+    get limiteCredito() {
+        return this.#limiteCredito;
+    }
+
+    set limiteCredito(novoLimite) {
+        if (novoLimite >= 0.0) {
+            this.#limiteCredito = novoLimite;
+        } else if (this.#limiteCredito == undefined) {
+            this.#limiteCredito = 100.0;
+        }
+    }
+
+    get categoria() {
+        return this.#categoria;
+    }
+
+    set categoria(novaCategoria) {
+        if (novaCategoria == "COMUM" || novaCategoria == "VIP" || novaCategoria == "PCD") {
+            this.#categoria = novaCategoria;
+        } else if (this.#categoria == undefined) {
+            this.#categoria = "COMUM";
+        }
+    }
+
+    toString() {
+        return (super.toString() +
+            "\nPontuação Fidelidade: " + this.#pontucaoFidelidade +
+            "\nLimite de Crédito: " + this.#limiteCredito.toFixed(2) +
+            "\nCategoria: " + this.#categoria);
+    }
+}
---
+++ conta.js
@@ -0,0 +1,57 @@
+import { Conta } from "./conta.js";
+
+export class Conta {
+
+  #nome;
+  #cpf;
+  #dataNascimento;
+
+  
+  constructor(nome, cpf, dataNascimento) {
+    this.#nome = nome.toUpperCase(); 
+    this.#cpf = cpf;
+
+    
+    if (dataNascimento != undefined && dataNascimento != "") {
+      this.#dataNascimento = dataNascimento;
+    } else {
+      this.#dataNascimento = "2000/01/01";
+    }
+  }
+
+  
+  get nome() {
+    return this.#nome;
+  }
+
+  
+  set nome(novoNome) {
+    if (novoNome != "") {
+      this.#nome = novoNome.toUpperCase();
+    }
+  }
+
+  
+  get cpf() {
+    return this.#cpf;
+  }
+
+  
+  get dataNascimento() {
+    return this.#dataNascimento;
+  }
+
+  set dataNascimento(novaData) {
+    if (novaData != "" && novaData.length == 10) {
+      this.#dataNascimento = novaData;
+    }
+  }
+
+  // Mostra as informações do cliente em formato de texto
+  toString() {
+    return "Nome: " + this.#nome +
+           "\nCPF: " + this.#cpf +
+           "\nData de Nascimento: " + this.#dataNascimento;
+  }
+}
+
